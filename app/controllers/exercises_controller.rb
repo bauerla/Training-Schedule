ebf6b622ef06f615e8f4211a9e6e672b63a4a53f
Lauri Bauer <lauri.bauer.dev@gmail.com>
2016-04-02 03:46:05 +0300

    Exercise update form and update actions - JS renders 'update_form' partial with animation on update button click & passes current exercise object to partial, Updated Events edit view to bootstrap form styling, application root view update to use Bootstrap grid system (can be used across whole application)

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -18,9 +18,20 @@ class ExercisesController < ApplicationController
     redirect_to event_path(@event)
   end
 
+  def update
+    @event = Event.find(params[:event_id])
+    @exercise = @event.exercises.find(params[:id])
+    if @exercise.update(exercise_params)
+      flash.keep[:success] = "Exercise updated!"
+      redirect_to event_url(@event)
+    else
+      flash.keep[:warning] = "Check your parameters!"
+    end
+  end
+
   def update_view
-    id = params[:ex_id] # Excercise id if needed
-    puts "Event:: " + id
+    @update_ex = Exercise.find(params[:ex_id])
+    puts "Event :: "+ @update_ex.inspect
     respond_to do |format|
       format.js
     end
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,5 +2,5 @@ class Exercise < ActiveRecord::Base
   belongs_to :event
 
   validates :desc, presence: true, length: { minimum: 5 }
-  validates :duration, :numericality => { only_integer: true, :greater_than_or_equal_to => 0 }
+  validates :duration, presence: true, :numericality => { only_integer: true, :greater_than_or_equal_to => 0 }
 end
