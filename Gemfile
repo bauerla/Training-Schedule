158c22bc716288114987fc5818325e35b304381a
bauerla <lauri.bauer.dev@gmail.com>
2017-05-14 14:26:38 +0300

    Faker plugin update + dummy (sample) data improvements

--- Gemfile
+++ Gemfile
@@ -54,7 +54,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
   # Creating test data
-  gem 'faker'
+  gem 'faker', '~> 1.7', '>= 1.7.3'
 end
 
 group :development do
@@ -64,3 +64,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+# Faker needs this on runtime
+gem 'i18n'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Change value according to whether want to print extra debug info
 debug = false
@@ -13,15 +9,62 @@ puts "-- Debugging on: #{debug} --"
 Event.destroy_all
 
 ## Add some Event and Exercise sample data
-# 		Creates new events using Faker helper gem
-# 		which generates random data to populate database objects.
+#     Creates new events using Faker helper gem
+# 		which generates random data to populate database.
+#     
 @evs = Array.new
 @exers = Array.new
 @comments = Array.new
 @odd = true
 
-@com_names = [Faker::StarWars, Faker::Superhero, Faker::Name]
-@com_types = [Faker::Hipster, Faker::Hacker, Faker::StarWars]
+@com_char = [
+  Proc.new { Faker::Ancient.god },
+  Proc.new { Faker::Ancient.hero },
+  Proc.new { Faker::Friends.character },
+  Proc.new { Faker::GameOfThrones.character },
+  Proc.new { Faker::HarryPotter.character },
+  Proc.new { Faker::LordOfTheRings.character },
+  Proc.new { Faker::StarWars.character },
+  Proc.new { Faker::StarWars.droid },
+  Proc.new { Faker::Superhero.name },
+  Proc.new { Faker::TwinPeaks.character}
+]
+
+@com_quote = [
+  Proc.new { Faker::ChuckNorris.fact },
+  Proc.new { Faker::Friends.quote },
+  Proc.new { Faker::Hacker.say_something_smart },
+  Proc.new { Faker::HarryPotter.quote },
+  Proc.new { Faker::StarWars.quote },
+  Proc.new { Faker::StarWars.wookie_sentence },
+  Proc.new { Faker::TwinPeaks.quote }
+]
+
+@com_location = [
+  Proc.new { Faker::Address.state },
+  Proc.new { Faker::Friends.location },
+  Proc.new { Faker::HarryPotter.location },
+  Proc.new { Faker::LordOfTheRings.location },
+  Proc.new { Faker::Pokemon.location },
+  Proc.new { Faker::StarWars.planet },
+  Proc.new { Faker::TwinPeaks.location }
+]
+
+@com_name = [
+  Proc.new { Faker::Name.name },
+  Proc.new { Faker::Pokemon.name },
+  Proc.new { Faker::RockBand.name },
+  Proc.new { Faker::Superhero.name }
+]
+
+@catch_phrase = [
+  Proc.new { Faker::Company.catch_phrase }
+]
+
+@vid_opts = [
+  ['Cortez the Killer', 'https://www.youtube.com/watch?v=6QjIHnb5Ivs'],
+  ['Pekka Pouta', 'https://www.youtube.com/watch?v=o6T9ESB_UcA']
+]
 
 #Create Events and child objects for them
 n = Faker::Number.between(30, 50)
@@ -35,7 +78,8 @@ n.times do
   	event = Event.create!(title: Faker::Team.sport.gsub(/\b\w/, &:upcase),
   												text: Faker::Shakespeare.as_you_like_it_quote,
   												starttime: stime,
-  												endtime: etime)
+  												endtime: etime
+    )
   else
     event = Event.create!(title: Faker::Team.sport.gsub(/\b\w/, &:upcase),
                           text: Faker::Shakespeare.as_you_like_it_quote,
@@ -44,8 +88,10 @@ n.times do
                           completed: true,
                           done_summary: Faker::Lorem.paragraph(2, true, 5),
                           done_additional: Faker::Company.catch_phrase,
-                          done_created_at: (etime + 2.hour))
+                          done_created_at: (etime + 2.hour)
+    )
   end
+  event.created_at = stime - 10.days
 	@evs.push(event) if debug
 
   # Create exercises for event
@@ -63,45 +109,55 @@ n.times do
   # Some Comments for Event
   n = Faker::Number.between(0, 10)
   n.times do
-    for i in 0..1
-      t = Faker::Number.between(0, 2)
-      if t == 0
-        commenter = Faker::Boolean.boolean ? @com_names[0].character : @com_names[0].droid if i.eql?(0)
-        comment_text = @com_types[0].paragraph(1, true, 5) if i.eql?(1)
-      elsif t == 1
-        commenter = @com_names[1].name if i.eql?(0)
-        comment_text = @com_types[1].say_something_smart if i.eql?(1)
-      else
-        commenter = @com_names[2].name if i.eql?(0)
-        comment_text = @com_types[2].quote if i.eql?(1)
-      end
-    end
-    #commenter = Faker::Name.name
-    #comment_text =  = Faker::Hipster.paragraph(1, true, 5)
+    commenter = @com_char[Faker::Number.between(0, @com_char.length - 1)].call
+    comment_text = @com_quote[Faker::Number.between(0, @com_quote.length - 1)].call
+    e_time = event.created_at
+    comment_time = Faker::Time.between(e_time, DateTime.now, :all)
+
     if debug
-      @comments.push(event.comments.create!(commenter: commenter, body: comment_text))
+      @comments.push(
+        event.comments.create!(
+          commenter: commenter,
+          body: comment_text,
+          created_at: comment_time
+        )
+      )
     else
       event.comments.create!(commenter: commenter, body: comment_text)
     end
   end
 
   # Video for every second Event
-  if @odd
-  	event.create_video!(link: 'https://www.youtube.com/watch?v=6QjIHnb5Ivs',
-	  										title: 'Cortez the Killer',
-	  										published_at: DateTime.now)
+  if Faker::Boolean.boolean
+    i = Faker::Number.between(0, @vid_opts.length - 1)
+  	event.create_video!(
+      title: @vid_opts[i][0],
+      link: @vid_opts[i][1],
+	  	published_at: DateTime.now
+    )
   end
   @odd ? @odd = false : @odd = true
 end
 
-# debugging & info
+## Debug info
 if debug
 	puts "Events:"
-	puts @evs.inspect
+  for e in @evs
+    puts e.inspect
+    puts ""
+  end
 	puts "----------------------------------------------------"
 	puts "Exercises:"
-	puts @exers.inspect
+  for ex in @exers
+    puts ex.inspect
+    puts ""
+  end
 	puts "----------------------------------------------------"
+  puts "Comments:"
+  for c in @comments
+    puts c.inspect
+    puts ""
+  end
 end
 
 puts "Created #{Event.count} events!"
@@ -109,3 +165,4 @@ puts "Created #{Exercise.count} exercises!"
 puts "Created #{Comment.count} comments!"
 puts "Created #{Video.count} videos!"
 puts ""
+
